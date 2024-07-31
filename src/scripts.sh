d519554a5299fc381b06167b4a5fd967cb948e78
nickmura <nickmuradev@gmail.com>
2024-07-30 19:09:52 -0600

    added scripts.sh

---
+++ src/scripts.sh
@@ -0,0 +1,3 @@
+python3 yt-dlp --downloader ffmpeg --downloader-args "ffmpeg:-c:v libx264 -crf 28 -preset medium"
+curl --location --request GET 'https://api.thetavideoapi.com
+#rest of commands in ./upload.js & ./vps.js
